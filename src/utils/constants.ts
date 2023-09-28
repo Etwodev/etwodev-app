c2e12bdc3ba4a089d28ae243d9dc4ea37b502665
etwodev <ewoods@adaptavist.com>
2023-09-28 19:18:09 +0100

    chore: implement base site

--- src/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const AppTexts = {
-  headerText: "Hello World",
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgrPlugin from "vite-plugin-svgr";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// see all documentation here https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build change as your need
-  // comment this out if that isn't relevant for your project
-  build: {
-    outDir: "build",
-  },
-  optimizeDeps: {
-    exclude: ['@react-three/fiber'],
-  },
-  plugins: [
-    react(),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true,
-        // ...svgr options (https://react-svgr.com/docs/options/)
-      },
-    }),
-  ],
-});
+  plugins: [react()],
+})
